a5924edaafab988987fa34e5bb6b7954e1687568
pilate <ashleyhwacha@gmail.com>
2022-06-05 17:24:49 +0200

    removing junk code

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ fetch('./data.json')
 .then(res => res.json())
 .then(data => {
 
- let largest = data[1].amount
-
  const newMarkup = data.map(day => {
   
-  if(day.amount > largest) { 
-   largest = day.amount
-  }
   const size = `${day.amount * 3}px`
   return `<p class="day">
   <span class="day-bar" style="height: ${size};"></span>
