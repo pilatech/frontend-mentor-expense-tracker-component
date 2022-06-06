98d75679ef978339a5e662fdbd2e17111bdd9001
pilate <pilatech6@gmail.com>
2022-06-06 02:33:23 +0200

    added styling for largets bar

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const container = document.querySelector('.widget-main-section')
 fetch('./data.json')
 .then(res => res.json())
 .then(data => {
-
+ const highest = Math.max(...data.map(item => item.amount))
  const newMarkup = data.map(day => {
   
   const size = `${day.amount * 3}px`
   return `<p class="day">
-  <span class="day-bar" style="height: ${size};"></span>
+  <span class="day-bar" style="height: ${size};  background: ${highest === day.amount && "hsl(186, 34%, 60%)"};"></span>
   sun
   </p>`
  })
